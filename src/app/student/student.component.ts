3ffa8c10ec81a7dcd2703873207cca5bdeb973d0
eProjetIngenieur <fatima.elmourtaki@e-polytechnique.ma>
2021-07-06 12:14:53 +0100

    first commit

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+  goToPage(pageName:string):void{
+    this.router.navigate([`${pageName}`]);
+  }
+}
